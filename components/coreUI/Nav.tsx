24b85bfbd20548d252bd4697f5cfdb14465b0ac1
detailscale <detailscale@icloud.com>
2025-06-10 20:40:41 +0700

    feat: create navbar

---
+++ components/coreUI/Nav.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+export default function Nav() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString("en-GB", {
+    timeZone: "Etc/GMT-7",
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  return (
+    <div className="font-outfit">
+      <nav className="grid h-8 flex-shrink-0 grid-cols-3 items-center bg-neutral-800 px-4 text-neutral-300 text-sm">
+        <div className="relative select-none">psord</div>
+        <div></div>
+        <div className="justify-self-end">{formattedTime}</div>
+      </nav>
+    </div>
+  );
+}
