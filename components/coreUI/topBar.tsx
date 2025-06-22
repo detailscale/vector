4f7593e50df3d2f8ed8cc235d9f57fcf74ce373e
pickingname <trintrinir@icloud.com>
2025-06-22 13:24:44 +0700

    feat: add topBar component

---
+++ components/coreUI/topBar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Bell, Search, ShoppingCart } from "lucide-react";
+
+export default function TopBar() {
+  return (
+    <main>
+      <div className="pt-5 max-w-2xl mx-auto">
+        <div className="flex justify-between items-center w-full">
+          <Button className="w-8 h-8 bg-[#171717] rounded-sm border border-[#2f2f2f] cursor-pointer hover:bg-[#262626] transition-all duration-150 ease-in-out">
+            <Search />
+          </Button>
+
+          <div className="flex gap-2">
+            <Button className="w-8 h-8 bg-[#171717] rounded-sm border border-[#2f2f2f] p-2 cursor-pointer hover:bg-[#262626] transition-all duration-150 ease-in-out">
+              <ShoppingCart />
+            </Button>
+            <Button className="w-8 h-8 bg-[#171717] rounded-sm border border-[#2f2f2f] p-2 cursor-pointer hover:bg-[#262626] transition-all duration-150 ease-in-out">
+              <Bell />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
