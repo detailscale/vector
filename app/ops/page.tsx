67e2003bb38d9e5fed4ebc1d49257081fdf59412
pickingname <trintrinir@icloud.com>
2025-09-15 17:58:42 +0700

    fix: rearrange paths

---
+++ app/ops/page.tsx
@@ -0,0 +1,13 @@
+import Nav from "@/components/coreUI/Nav";
+import React from "react";
+
+export default function Page() {
+  return (
+    <main>
+      <Nav />
+      <div id="CONTENT" className="blp_prop">
+        asdf
+      </div>
+    </main>
+  );
+}
